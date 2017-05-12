186e9fcfb80b499487a9b28bb0a9b702e26930d2
joeyappelhans <9jappelhans@stpatrick.org>
2017-05-12 13:52:27 -0500

    started physics

---
+++ physics.js
@@ -0,0 +1,16 @@
+function distance(v,t){
+    let distance=v*t;
+    return distance;
+}
+function force(m,a){
+    let force=m*a;
+    return force;
+}
+function momentum(m,v){
+    let impulse=m*v;
+    return impluse;
+}
+function ke(m,v){
+    let energy=m*v*v/2;
+    return energy;
+}
