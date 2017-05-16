85fd23a3a3679c2f6560d72774110c9da123b942
joeyappelhans <9jappelhans@stpatrick.org>
2017-05-16 13:54:40 -0500

    worked on motion and energy

--- physics.js
+++ physics.js
@@ -1,14 +1,23 @@
+const motion = require('./motion');
+const energy = require('./energy');
+const readline = require('readline');
+
+
+const terminal = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+terminal.question("What type of equation are you looking for?",function(str){
+
+    //you will configure your series of questions inside here.
+
+
+});
+
 function distance(v,t){
     let distance=v*t;
     return distance;
-}
-function force(m,a){
-    let force=m*a;
-    return force;
-}
-function momentum(m,v){
-    let impulse=m*v;
-    return impluse;
 }
 function ke(m,v){
     let energy=m*v*v/2;
